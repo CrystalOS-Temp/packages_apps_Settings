4fea86f28db8ee44e1526a62d54d1621b3c09597
Alex Cruz <du.alexcruz@gmail.com>
2020-06-25 14:02:16 -0400

    Settings: Make lockscreen timeout configurable [2/2]
    Modeled after the screen timeout, this will control the lockscreen
    timeout.
    
    Change-Id: I5e095ee1c2fa59ab715964ca7ac0cd2f4a4b3ab7
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -29,6 +29,7 @@ import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
+import com.android.settings.display.TimeoutLockscreenPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.EmulateDisplayCutoutPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -42,6 +43,8 @@ import java.util.List;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
+
+    private static final String KEY_LOCKSCREEN_TIMEOUT = "lockscreen_timeout";
 
     @Override
     public int getMetricsCategory() {
@@ -80,6 +83,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new TapToWakePreferenceController(context));
+        controllers.add(new TimeoutLockscreenPreferenceController(context, KEY_LOCKSCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
