LeafOS-Project/android_packages_apps_Settings

SoftAp: Add Hidden SSID preference
[jhonboy121]: adapted to A13

Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
Change-Id: I8a222a9a9ae8f005953b738368168d551a502491
Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

src/com/android/settings/AllInOneTetherSettings.java
@@ -62,6 +62,8 @@ import com.android.settingslib.TetherUtil;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.search.SearchIndexable;
 
+import ink.kscope.settings.wifi.tether.WifiTetherHiddenSsidPreferenceController;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
@@ -89,6 +91,8 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
             "wifi_tether_network_ap_band" + DEDUP_POSTFIX;
     @VisibleForTesting
     static final String KEY_WIFI_TETHER_SECURITY = "wifi_tether_security" + DEDUP_POSTFIX;
+    @VisibleForTesting
+    static final String KEY_WIFI_TETHER_HIDDEN_SSID = WifiTetherHiddenSsidPreferenceController.PREF_KEY + DEDUP_POSTFIX;
 
     private static final String KEY_DATA_SAVER_FOOTER = "disabled_on_data_saver" + DEDUP_POSTFIX;
     private static final String KEY_WIFI_TETHER_GROUP = "wifi_tether_settings_group";
@@ -119,6 +123,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
     private WifiTetherPasswordPreferenceController mPasswordPreferenceController;
     private WifiTetherApBandPreferenceController mApBandPreferenceController;
     private WifiTetherSecurityPreferenceController mSecurityPreferenceController;
+    private WifiTetherHiddenSsidPreferenceController mHiddenSsidPreferenceController;
     private PreferenceGroup mWifiTetherGroup;
     private boolean mShouldShowWifiConfig = true;
     private boolean mHasShownAdvance;
@@ -187,6 +192,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         mSecurityPreferenceController = use(WifiTetherSecurityPreferenceController.class);
         mPasswordPreferenceController = use(WifiTetherPasswordPreferenceController.class);
         mApBandPreferenceController = use(WifiTetherApBandPreferenceController.class);
+        mHiddenSsidPreferenceController = use(WifiTetherHiddenSsidPreferenceController.class);
         getSettingsLifecycle().addObserver(use(UsbTetherPreferenceController.class));
         getSettingsLifecycle().addObserver(use(BluetoothTetherPreferenceController.class));
         getSettingsLifecycle().addObserver(use(EthernetTetherPreferenceController.class));
@@ -335,6 +341,8 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         controllers.add(
                 new WifiTetherAutoOffPreferenceController(context, KEY_WIFI_TETHER_AUTO_OFF));
         controllers.add(
+                new WifiTetherHiddenSsidPreferenceController(context, listener));
+        controllers.add(
                 new WifiTetherFooterPreferenceController(context));
 
         return controllers;
@@ -380,6 +388,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
                     SoftApConfiguration.SECURITY_TYPE_WPA2_PSK);
         }
         configBuilder.setBand(mApBandPreferenceController.getBandIndex());
+        configBuilder.setHiddenSsid(mHiddenSsidPreferenceController.isHiddenSsidEnabled());
         return configBuilder.build();
     }
 
@@ -388,6 +397,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         mSecurityPreferenceController.updateDisplay();
         mPasswordPreferenceController.updateDisplay();
         mApBandPreferenceController.updateDisplay();
+        mHiddenSsidPreferenceController.updateDisplay();
     }
 
     @Override

src/ink/kscope/settings/wifi/tether/WifiTetherHiddenSsidPreferenceController.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ink.kscope.settings.wifi.tether;
+
+import android.content.Context;
+import android.net.wifi.SoftApConfiguration;
+import android.util.FeatureFlagUtils;
+
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.core.FeatureFlags;
+import com.android.settings.wifi.tether.WifiTetherBasePreferenceController;
+
+/**
+ * This controller helps to manage the state of hide SSID switch preference.
+ */
+public class WifiTetherHiddenSsidPreferenceController extends
+        WifiTetherBasePreferenceController {
+
+    public static final String DEDUP_POSTFIX = "_2";
+    public static final String PREF_KEY = "wifi_tether_hidden_ssid";
+
+    private boolean mHiddenSsid;
+
+    public WifiTetherHiddenSsidPreferenceController(Context context,
+            WifiTetherBasePreferenceController.OnTetherConfigUpdateListener listener) {
+        super(context, listener);
+
+        if (mWifiManager != null) {
+            final SoftApConfiguration config = mWifiManager.getSoftApConfiguration();
+            if (config != null) {
+                mHiddenSsid = config.isHiddenSsid();
+            }
+        }
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return FeatureFlagUtils.isEnabled(mContext, FeatureFlags.TETHER_ALL_IN_ONE)
+                ? PREF_KEY + DEDUP_POSTFIX : PREF_KEY;
+    }
+
+    @Override
+    public void updateDisplay() {
+        if (mPreference == null) {
+            return;
+        }
+        ((SwitchPreference) mPreference).setChecked(mHiddenSsid);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        mHiddenSsid = (Boolean) newValue;
+        if (mListener != null) {
+            mListener.onTetherConfigUpdated(this);
+        }
+        return true;
+    }
+
+    public boolean isHiddenSsidEnabled() {
+        return mHiddenSsid;
+    }
+}

CrystalOS-Temp/packages_apps_Settings

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/android/settings/AllInOneTetherSettings.java
@@ -55,6 +55,7 @@ import com.android.settings.wifi.tether.WifiTetherApBandPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherAutoOffPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherBasePreferenceController;
 import com.android.settings.wifi.tether.WifiTetherFooterPreferenceController;
+import com.android.settings.wifi.tether.WifiTetherHiddenSsidPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherPasswordPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherSSIDPreferenceController;
 import com.android.settings.wifi.tether.WifiTetherSecurityPreferenceController;
@@ -89,6 +90,8 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
             "wifi_tether_network_ap_band" + DEDUP_POSTFIX;
     @VisibleForTesting
     static final String KEY_WIFI_TETHER_SECURITY = "wifi_tether_security" + DEDUP_POSTFIX;
+    @VisibleForTesting
+    static final String KEY_WIFI_TETHER_HIDDEN_SSID = WifiTetherHiddenSsidPreferenceController.PREF_KEY + DEDUP_POSTFIX;
 
     private static final String KEY_DATA_SAVER_FOOTER = "disabled_on_data_saver" + DEDUP_POSTFIX;
     private static final String KEY_WIFI_TETHER_GROUP = "wifi_tether_settings_group";
@@ -119,6 +122,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
     private WifiTetherPasswordPreferenceController mPasswordPreferenceController;
     private WifiTetherApBandPreferenceController mApBandPreferenceController;
     private WifiTetherSecurityPreferenceController mSecurityPreferenceController;
+    private WifiTetherHiddenSsidPreferenceController mHiddenSsidPreferenceController;
     private PreferenceGroup mWifiTetherGroup;
     private boolean mShouldShowWifiConfig = true;
     private boolean mHasShownAdvance;
@@ -187,6 +191,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         mSecurityPreferenceController = use(WifiTetherSecurityPreferenceController.class);
         mPasswordPreferenceController = use(WifiTetherPasswordPreferenceController.class);
         mApBandPreferenceController = use(WifiTetherApBandPreferenceController.class);
+        mHiddenSsidPreferenceController = use(WifiTetherHiddenSsidPreferenceController.class);
         getSettingsLifecycle().addObserver(use(UsbTetherPreferenceController.class));
         getSettingsLifecycle().addObserver(use(BluetoothTetherPreferenceController.class));
         getSettingsLifecycle().addObserver(use(EthernetTetherPreferenceController.class));
@@ -335,6 +340,8 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         controllers.add(
                 new WifiTetherAutoOffPreferenceController(context, KEY_WIFI_TETHER_AUTO_OFF));
         controllers.add(
+                new WifiTetherHiddenSsidPreferenceController(context, listener));
+        controllers.add(
                 new WifiTetherFooterPreferenceController(context));
 
         return controllers;
@@ -380,6 +387,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
                     SoftApConfiguration.SECURITY_TYPE_WPA2_PSK);
         }
         configBuilder.setBand(mApBandPreferenceController.getBandIndex());
+        configBuilder.setHiddenSsid(mHiddenSsidPreferenceController.isHiddenSsidEnabled());
         return configBuilder.build();
     }
 
@@ -388,6 +396,7 @@ public class AllInOneTetherSettings extends RestrictedDashboardFragment
         mSecurityPreferenceController.updateDisplay();
         mPasswordPreferenceController.updateDisplay();
         mApBandPreferenceController.updateDisplay();
+        mHiddenSsidPreferenceController.updateDisplay();
     }
 
     @Override

src/com/android/settings/wifi/tether/WifiTetherHiddenSsidPreferenceController.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.wifi.tether;
+
+import android.content.Context;
+import android.net.wifi.SoftApConfiguration;
+import android.util.FeatureFlagUtils;
+
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.core.FeatureFlags;
+import com.android.settings.wifi.tether.WifiTetherBasePreferenceController;
+
+/**
+ * This controller helps to manage the state of hide SSID switch preference.
+ */
+public class WifiTetherHiddenSsidPreferenceController extends
+        WifiTetherBasePreferenceController {
+
+    public static final String DEDUP_POSTFIX = "_2";
+    public static final String PREF_KEY = "wifi_tether_hidden_ssid";
+
+    private boolean mHiddenSsid;
+
+    public WifiTetherHiddenSsidPreferenceController(Context context,
+            WifiTetherBasePreferenceController.OnTetherConfigUpdateListener listener) {
+        super(context, listener);
+
+        if (mWifiManager != null) {
+            final SoftApConfiguration config = mWifiManager.getSoftApConfiguration();
+            if (config != null) {
+                mHiddenSsid = config.isHiddenSsid();
+            }
+        }
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return FeatureFlagUtils.isEnabled(mContext, FeatureFlags.TETHER_ALL_IN_ONE)
+                ? PREF_KEY + DEDUP_POSTFIX : PREF_KEY;
+    }
+
+    @Override
+    public void updateDisplay() {
+        if (mPreference == null) {
+            return;
+        }
+        ((SwitchPreference) mPreference).setChecked(mHiddenSsid);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        mHiddenSsid = (Boolean) newValue;
+        if (mListener != null) {
+            mListener.onTetherConfigUpdated(this);
+        }
+        return true;
+    }
+
+    public boolean isHiddenSsidEnabled() {
+        return mHiddenSsid;
+    }
+}